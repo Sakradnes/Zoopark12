ed2235b762c585b8412956c03e6d524b67e02f2b
Alex <sakradnes@mail.ru>
2023-11-22 18:12:37 +0300

    component Layot

---
+++ components/Layout.jsx
@@ -0,0 +1,23 @@
+const React = require('react');
+
+module.exports = function Layout({ title, children }) {
+  return (
+    <html lang="ru">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="/css/style.css" />
+
+        <title>{title}</title>
+      </head>
+
+      <body>{children}</body>
+    </html>
+  );
+};
