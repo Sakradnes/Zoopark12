2df2f9531175ffe226cfeedcb270e0564c8108ee
admin <admin@MacBook-Pro.local>
2023-11-23 16:23:41 +0300

    post

--- routes/api/animalsApi.route.js
+++ routes/api/animalsApi.route.js
@@ -2,5 +2,9 @@ const router = require('express').Router();
 const { Animal, ImgAnimals } = require('../../db/models');
 
 
-
+router.post('/', async (req,res)=>{
+const newAnimal = await Animal.create({
+    
+})
+})
 module.exports = router;
